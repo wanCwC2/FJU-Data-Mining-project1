596b2be3f24aad5256b23ab4461ff9aab1727cf5
wanCwC2 <WanPcwc0630@gmail.com>
2022-11-21 17:34:58 +0800

    Dvide the data into validation and test sets

--- Main.py
+++ Main.py
@@ -4,8 +4,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import NearestCentroid
 warnings.filterwarnings('ignore')
 
+#Read data
 data = pd.read_csv('data/project1_train.csv')
 test = pd.read_csv('data/project1_test.csv')
+
+#Find analysis target
+X_df = pd.DataFrame(data, columns = ['Age', 'Gender', 'Total_Bilirubin', 'Direct_Bilirubin',
+       'Alkaline_Phosphotase', 'Alamine_Aminotransferase',
+       'Aspartate_Aminotransferase', 'Total_Protiens', 'Albumin',
+       'Albumin_and_Globulin_Ratio'])
+y_df = pd.DataFrame(data, columns = ['Label'])
+
+#Dvide the data into validation and test sets
+X_train, X_valid, y_train, y_valid = train_test_split(X_df, y_df, train_size=0.6, random_state=0)
+X_valid, X_test, y_valid, y_test = train_test_split(X_valid, y_valid, train_size=0.5, random_state=0)
