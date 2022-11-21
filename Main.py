0eed7a7fab18e171789dafaf0d090535a5bd7698
wanCwC2 <WanPcwc0630@gmail.com>
2022-11-22 05:50:30 +0800

    Standardize but Have something wrong

--- Main.py
+++ Main.py
@@ -6,11 +6,15 @@ import warnings
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import NearestCentroid
+from sklearn.preprocessing import StandardScaler
 warnings.filterwarnings('ignore')
 
 #Read data
 data = pd.read_csv('data/project1_train.csv')
 test = pd.read_csv('data/project1_test.csv')
+
+#Reviset Male, Female
+data.loc[:, 'Gender'] = ['Male', 1]
 
 #Find analysis target
 X_df = pd.DataFrame(data, columns = ['Age', 'Gender', 'Total_Bilirubin', 'Direct_Bilirubin',
@@ -22,3 +26,14 @@ y_df = pd.DataFrame(data, columns = ['Label'])
 #Dvide the data into validation and test sets
 X_train, X_valid, y_train, y_valid = train_test_split(X_df, y_df, train_size=0.6, random_state=0)
 X_valid, X_test, y_valid, y_test = train_test_split(X_valid, y_valid, train_size=0.5, random_state=0)
+
+#Standardization
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_valid_std = sc.transform(X_valid)
+X_test_std = sc.transform(X_test)
+
+sc_test = StandardScaler()
+sc_test.fit(test)
+test_std = sc_test.transform(test)
