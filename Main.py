406576ddd212983e3f890bc3c178cf5cfb451583
wanCwC2 <WanPcwc0630@gmail.com>
2022-11-24 08:04:00 +0800

    Change numay array to pandas dataframe

--- Main.py
+++ Main.py
@@ -39,15 +39,20 @@ X_train_std = sc.transform(X_train)
 X_valid_std = sc.transform(X_valid)
 X_test_std = sc.transform(X_test)
 
+#Change numpy array to pandas
+X_train_std = pd.DataFrame(X_train_std, index=X_train.index, columns=X_train.columns)
+
 #Delete too big data
 #X_train_std.drop(X_train_std[X_train_std[:,5]>3].index) #pandas data, but change to array after standardizating
-X_train_std = np.delete(X_train_std, X_train_std[:,5]>3, axis=0)
-X_valid_std = np.delete(X_valid_std, X_valid_std[:,5]>3, axis=0)
+for i in range(0, X_train_std.shape[1]):
+    y_train.drop(np.where(X_train_std[:,5]>2)[0])
+    X_train_std = np.delete(X_train_std, X_train_std[:,i]>2, axis=0)
+    X_valid_std = np.delete(X_valid_std, X_valid_std[:,i]>2, axis=0)
 
 sc_test = StandardScaler()
 sc_test.fit(test)
 test_std = sc_test.transform(test)
-
+'''
 #多層感知器 Multi-layer Perceptron / 類神經網路 Neural network
 model = MLPClassifier(
   hidden_layer_sizes=(10),
@@ -58,3 +63,17 @@ model = MLPClassifier(
 model.fit(X_train_std, y_train)
 print("Training set score: %f" % model.score(X_train_std, y_train))
 print("Validation set score: %f" % model.score(X_valid_std, y_valid))
+
+# Decision Tree
+from sklearn.tree import DecisionTreeRegressor
+maxRate = 0
+indexRate = 0
+for i in range (1,30):
+    model = DecisionTreeRegressor(random_state = i)
+    model.fit(X_train_std, y_train)
+    if maxRate < model.score(X_valid_std, y_valid):
+        maxRate = model.score(X_valid_std, y_valid)
+        indexRate = i
+DecisionTreeRegressor(random_state = indexRate)
+print("Correct rate using Decision Tree: ", round(model.score(X_test_std, y_test),5))
+'''
